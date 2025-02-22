920f956d324906bf162caab2d77c3698dfdf4725
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-02-23 03:36:02 +0700

    Initial commit

---
+++ myfirstfsmodule.js
@@ -0,0 +1,48 @@
+// Import modules
+var http = require("http");
+var fs = require("fs");
+
+// Create http server using Node.js
+http
+  .createServer(function (req, res) {
+    // Read file using file system module method readFile()
+    fs.readFile("demofile1.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
+
+// Create file using appendFile()
+fs.appendFile("mynewfile1.txt", "Hello content!", function (err) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+// Create file using open()
+fs.open("mynewfile2.txt", "w", function (err, file) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+// Create file using writeFile()
+fs.writeFile("mynewfile3.txt", "Hello content!", function (err) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+
+// Update file using appendFile()
+fs.appendFile("mynewfile1.txt", " This is my text.", function (err) {
+  if (err) throw err;
+  console.log("Updated!");
+});
+// Update file using writeFile()
+fs.writeFile("mynewfile3.txt", "this is my text", function (err) {
+  if (err) throw err;
+  console.log("Replaced!");
+});
+
+// Delete file using unlink()
+fs.unlink("mynewfile2.txt", function (err) {
+  if (err) throw err;
+  console.log("File deleted!");
+});
